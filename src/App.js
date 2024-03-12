af143f34033747160ec7325be880296493fe2b7c
AjaySRawat07 <ajju246455@gmail.com>
2024-03-12 23:02:48 +0530

    main

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import InputContainer from './components/InputContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <InputContainer/>
+    </div>
+  );
+}
+
+export default App;
